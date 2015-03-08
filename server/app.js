63d8b9b37611e6a048f8fbd27a27e452a3c075c9
Andrew <champstarr@gmail.com>
2015-03-07 19:18:38 -1000

    implemmenting post request

--- server/app.js
+++ server/app.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-
+var data = 'aloha'
 app.use(express.static(__dirname + "/../public"));//points to static files to simplify pathing
 app.use(bodyParser.urlencoded({extended: true})); //send through body-parser function for cleaner formatting
 
 app.get('/',function(req,res){
   res.render(__dirname + "/../public/index.html");
+})
+
+app.post('/', function(req,res){
+  console.log(data);
 })
 
 var server = app.listen (3000, function(){
