d70233b5617fdbf947adbd3a6c3b5dd2e515e220
Andrew <champstarr@gmail.com>
2015-03-07 16:58:03 -1000

    added middleware, app.get route, and git ignore

--- server/app.js
+++ server/app.js
@@ -1,1 +1,15 @@
 var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + "/../public"));//points to static files to simplify pathing
+app.use(bodyParser.urlencoded({extended: true})); //send through body-parser function for cleaner formatting
+
+app.get('/',function(req,res){
+  res.render(__dirname + "/../public/index.html");
+})
+
+var server = app.listen (3000, function(){
+  var host = server.address().address
+  var port = server.address().port
+})
